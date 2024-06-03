0aae3d6e3ebc118eca5c0a7623e5f083d9f0a9fd
hypad590 <aloxaryabov@gmail.com>
2024-06-03 18:12:35 +0300

    ?

--- src/main/java/com/hypad/LibraryManagementSystem/controller/UsersController.java
+++ src/main/java/com/hypad/LibraryManagementSystem/controller/UsersController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/api/v2")
 @AllArgsConstructor
